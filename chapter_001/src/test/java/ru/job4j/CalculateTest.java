c89eb3318c5872605d3f3ca94ef6680da3d1f150
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-04-15 12:48:18 +0300

    1.4. JUnit

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,24 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+@author Romantsov Aleksey
+*/
+public class CalculateTest {
+	/**
+	* Test echo.
+	*/ @Test
+	public void whenTakeNameThenTreeEchoPlusName() {
+		String input = "Aleksey Romantsov";
+		String expect = "Echo, echo, echo : Aleksey Romantsov"; 
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for test calculate task.
+*
+* @author Aleksey Romantsov
+* @version 1.0.0.0
+* @since 15.04.2018
+*/
+package ru.job4j;
