ad22b5d33461bfe4c0c2e939769c24144790d086
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-04-15 11:44:19 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,14 @@
 package ru.job4j;
-
+/**
+This is test-class for first program which output Hello world to console
+@author Romantsov Aleksey
+*/
 public class Calculate{
-	public static void main(String[] args){
+	/**
+	method main
+	@param args - array of string params
+	*/
+	public static void main(String[] args) {
 		System.out.println("Hello world!");
 	}
 }
