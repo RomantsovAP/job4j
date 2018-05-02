a78ffe7507a96a4401d5b53c6c2a685b18070d2f
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-05-02 22:56:07 +0300

    5. Рефакторинг теста @Before @After

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,43 +1,66 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream fakeConsole = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.fakeConsole));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+
     @Test
     public void whenTriangleDrawThenItOutputTriangleToConsole() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream fakeConsole = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(fakeConsole));
-        String result = "*" + System.lineSeparator()
-                        + "**" + System.lineSeparator()
-                        + "***" + System.lineSeparator()
-                        + "****"  + System.lineSeparator();
+        String result = new StringBuilder()
+                .append("*")
+                .append(System.lineSeparator())
+                .append("**")
+                .append(System.lineSeparator())
+                .append("***")
+                .append(System.lineSeparator())
+                .append("****")
+                .append(System.lineSeparator())
+                .toString();
 
         Paint paint = new Paint();
         paint.draw(new Triangle());
-        assertThat(result, is(fakeConsole.toString()));
-        System.setOut(stdout);
+        assertThat(result, is(this.fakeConsole.toString()));
     }
 
     @Test
     public void whenSquareDrawThenItOutputSquareToConsole() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream fakeConsole = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(fakeConsole));
-        String result = "****" + System.lineSeparator()
-                + "****" + System.lineSeparator()
-                + "****" + System.lineSeparator()
-                + "****"  + System.lineSeparator();
+        String result = new StringBuilder()
+                .append("****")
+                .append(System.lineSeparator())
+                .append("****")
+                .append(System.lineSeparator())
+                .append("****")
+                .append(System.lineSeparator())
+                .append("****")
+                .append(System.lineSeparator())
+                .toString();
 
         Paint paint = new Paint();
         paint.draw(new Square());
-        assertThat(result, is(fakeConsole.toString()));
-        System.setOut(stdout);
+        assertThat(result, is(this.fakeConsole.toString()));
     }
 }
