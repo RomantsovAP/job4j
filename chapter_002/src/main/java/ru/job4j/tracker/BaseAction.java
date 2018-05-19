d860d1a0094da554296f27563a219ccd987407fe
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-05-19 21:45:09 +0300

    1. Рефакторинг - Перенести общие методы в абстрактный класс

--- chapter_002/src/main/java/ru/job4j/tracker/BaseAction.java
+++ chapter_002/src/main/java/ru/job4j/tracker/BaseAction.java
@@ -1,5 +1,11 @@
 package ru.job4j.tracker;
 
+/**
+ * Класс-шаблон для любого будущего действия пользователя
+ * @author AlekseyRomantsov
+ * @since 19.05.2018
+ * @version 1.0.0.0
+ */
 public abstract class BaseAction implements UserAction {
     private final int key;
     private final String name;
@@ -9,11 +15,19 @@ public abstract class BaseAction implements UserAction {
         this.name = name;
     }
 
+    /**
+     * Возвращает ключ - целое число, порядковый номер пункта меняю
+     * @return - число, порядковый номер пункта меню
+     */
     @Override
     public int key() {
         return this.key;
     }
 
+    /**
+     * Показать описание пункта меню
+     * @return - строковое описание пункта меню
+     */
     @Override
     public String showInfo() {
         return String.format("%s. %s", this.key, this.name);
