81808fdbc33b69246280454f34275cffc2dbdfb7
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-06-06 21:37:15 +0300

    Слияние упорядоченных массивов

--- chapter_002/src/main/java/ru/job4j/sort/ArraysMerge.java
+++ chapter_002/src/main/java/ru/job4j/sort/ArraysMerge.java
@@ -14,14 +14,14 @@ public class ArraysMerge {
      * @return - отсортированный массив, полученный слиянием
      */
     public int[] merge(int[] a, int[] b) {
-        int[] result = new int [a.length + b.length];
+        int[] result = new int[a.length + b.length];
         int i = 0, j = 0;
-        while ( i + j < a.length + b.length) {
+        while (i + j < a.length + b.length) {
             if (i < a.length && j < b.length) {
                 result[i + j] = (a[i] > b[j]) ? b[j++] : a[i++];
             } else if (i == a.length) {
                 result[i + j] = b[j++];
-            } else if (j == b.length){
+            } else if (j == b.length) {
                 result[i + j] = a[i++];
             }
         }
