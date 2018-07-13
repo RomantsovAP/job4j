9b821496d609fdedefb715b2f5481a429d3eb2ae
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-07-14 00:00:34 +0300

    5.3.1. Создать динамический список на базе массива.

---
+++ chapter_003/src/main/java/ru/job4j/lists/DynamicArrayList.java
@@ -0,0 +1,76 @@
+package ru.job4j.lists;
+
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Динамический расширяемый массив
+ * @author AlekseyRomantsov
+ * @version 1.0.0.0
+ * @since 13.07.2018
+ * @param <E> - тип хранимых значений
+ */
+public class DynamicArrayList<E> implements Iterable<E> {
+
+    private Object[] container = new Object[1];
+    private int modCount;
+    private int size;
+
+    private void checkSize() {
+        if (size == container.length) {
+            container = Arrays.copyOf(container, container.length * 2);
+        }
+    }
+
+    /**
+     * Добавление элемента
+     * @param value - новое значение
+     */
+    public void add(E value) {
+        checkSize();
+        container[size++] = value;
+        modCount++;
+    }
+
+    /**
+     * Получение элемента по индексу
+     * @param index - индекс, если индекс выходит за границы массива
+     *             - будет выброшено исключение, ArrayIndexOutOfBoundsException
+     * @return - полученное значение
+     */
+    public E get(int index) {
+        if (index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return (E)container[index];
+    }
+
+    /**
+     * Итератор
+     * @return - итератор значений динамического массива
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int position;
+            private final int expectedModCount = modCount;
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw  new ConcurrentModificationException();
+                }
+                return position < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (E)container[position++];
+            }
+        };
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/lists/DynamicArrayListTest.java
@@ -0,0 +1,62 @@
+package ru.job4j.lists;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DynamicArrayListTest {
+
+    private DynamicArrayList<Integer> list;
+
+    @Before
+    public void beforeTest() {
+        list = new DynamicArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        assertThat(list.get(2), is(3));
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void whenGetForthElementFormThreeElementListItThrowsExc(){
+        list.get(3);
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenAddElementWhileIterateItThrowsExc() {
+        for (Integer i: list) {
+            if (i == 1) {
+                list.add(4);
+            }
+        }
+    }
+
+    @Test
+    public void whenIteratethenItWorks() {
+        Iterator<Integer> it = list.iterator();
+        it.hasNext();
+        it.hasNext();
+        it.hasNext();
+        int i = it.next();
+        assertThat(1, is(i));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenItearteAfterTheEndItThrowsExc() {
+        Iterator<Integer> it = list.iterator();
+        it.next();
+        it.next();
+        it.next();
+        it.next();
+    }
+}
