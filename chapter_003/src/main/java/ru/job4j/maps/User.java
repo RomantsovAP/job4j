47749260d3dab56db5dc36fdb7fac0e9f0b40464
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-07-21 20:15:48 +0300

    1. Создать модель User

---
+++ chapter_003/src/main/java/ru/job4j/maps/User.java
@@ -0,0 +1,31 @@
+package ru.job4j.maps;
+
+import java.util.Calendar;
+
+/**
+ * Пользователь, сотрудник
+ * @author AlekseyRomantsov
+ * @version 1.0.0.0
+ * @since 21.07.2018
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    /**
+     * @param name - ФИО
+     * @param children - количество детей
+     * @param birthday - дата рождения
+     */
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name='" + name + '\'' + ", birthday=" + birthday + '}';
+    }
+}
