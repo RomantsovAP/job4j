f41235400a57c72ce63eb95919e0956a2c626b75
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2018-07-21 22:41:27 +0300

    5. Перекрывать и equals и hashCode

--- chapter_003/src/test/java/ru/job4j/maps/UserTest1.java
+++ chapter_003/src/test/java/ru/job4j/maps/UserTest1.java
@@ -15,17 +15,19 @@ public class UserTest1 {
         Map<User, String> userMap = new HashMap<>();
 
         User user1 = new User("Иванов Иван Иванович", 1, new GregorianCalendar(1972, 7, 17));
-        System.out.println(user1.hashCode());
+        System.out.println(System.identityHashCode(user1));
         User user2 = new User("Иванов Иван Иванович", 1, new GregorianCalendar(1972, 7, 17));
-        System.out.println(user2.hashCode());
+        System.out.println(System.identityHashCode(user2));
         userMap.put(user1, "Biography of user1");
         userMap.put(user2, "Biography if user2");
 
         for (Map.Entry<User, String> entry : userMap.entrySet()) {
             System.out.println(entry.getKey().toString() + " --  " + entry.getValue());
+            System.out.println(System.identityHashCode(entry.getKey()));
         }
 
         assertThat(userMap.get(user1), is("Biography if user2"));
+
 
     }
 }
