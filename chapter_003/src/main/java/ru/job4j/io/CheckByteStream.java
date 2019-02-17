af554adacd131584ee1d28bd4892c4cf57596b04
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2019-02-17 22:29:25 +0300

    2. Удаление запрещенных слов

--- chapter_003/src/main/java/ru/job4j/io/CheckByteStream.java
+++ chapter_003/src/main/java/ru/job4j/io/CheckByteStream.java
@@ -1,10 +1,25 @@
 package ru.job4j.io;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.Scanner;
 
 public class CheckByteStream {
     boolean isNumber(InputStream in) throws IOException {
         return in.available() > 0 && in.read() % 2 == 0;
+    }
+
+    void dropAbuses(InputStream in, OutputStream out, String[] abuse) throws IOException {
+        Scanner scanner = new Scanner(in);
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            for (String wrongString : abuse) {
+                line = line.replaceAll(wrongString, "");
+            }
+            if (scanner.hasNext()) {
+                line += System.lineSeparator();
+            }
+            out.write(line.getBytes());
+        }
+        out.flush();
     }
 }
--- chapter_003/src/test/java/ru/job4j/io/CheckByteStreamTest.java
+++ chapter_003/src/test/java/ru/job4j/io/CheckByteStreamTest.java
@@ -1,11 +1,23 @@
 package ru.job4j.io;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
+
 public class CheckByteStreamTest {
+
+    private String data;
+
+    @Before
+    public void init() {
+        data = "Однажды в студеную зимнюю пору" + System.lineSeparator() + "я из лесу вышел, был сильный мороз"
+                + System.lineSeparator() + "смотрю поднимается медленно в гору" + System.lineSeparator()
+                + "лошадка везущая хворосту воз";
+    }
 
     @Test
     public void whenEvenNumberInStreamThenTrue() {
@@ -36,4 +48,30 @@ public class CheckByteStreamTest {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void whenNoAbuseInStreamThenInEqualsOut() {
+        CheckByteStream checkByteStream = new CheckByteStream();
+        try (InputStream in = new ByteArrayInputStream(data.getBytes());
+             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            checkByteStream.dropAbuses(in, out, new String[0]);
+            Assert.assertArrayEquals(out.toByteArray(), data.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void whenSomeAbuseInStreamThenInEqualsOutWithoutAbuse() {
+        CheckByteStream checkByteStream = new CheckByteStream();
+        try (InputStream in = new ByteArrayInputStream(data.getBytes());
+             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+            checkByteStream.dropAbuses(in, out, new String[]{"зимнюю", "хворосту"});
+            Assert.assertArrayEquals(out.toByteArray(),
+                    data.replaceAll("зимнюю", "").replaceAll("хворосту", "").getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
