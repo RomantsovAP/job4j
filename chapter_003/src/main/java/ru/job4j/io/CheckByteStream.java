d67ad05e55f45eef0409784e04325d361c2db37d
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2019-03-11 21:34:09 +0300

    Два варианта исключения плохих слов используя StreamApi

--- chapter_003/src/main/java/ru/job4j/io/CheckByteStream.java
+++ chapter_003/src/main/java/ru/job4j/io/CheckByteStream.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class CheckByteStream {
     boolean isNumber(InputStream in) throws IOException {
@@ -21,5 +23,30 @@ public class CheckByteStream {
             out.write(line.getBytes());
         }
         out.flush();
+    }
+
+    void dropAbuses_mk2(InputStream in, OutputStream out, final String[] abuse) throws IOException {
+        Scanner scanner = new Scanner(in);
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+
+            line = Arrays.stream(line.split(" ")).filter(s -> !Arrays.stream(abuse).anyMatch(s1 -> s1.equals(s))).collect(Collectors.joining(" "));
+
+            if (scanner.hasNext()) {
+                line += System.lineSeparator();
+            }
+            out.write(line.getBytes());
+        }
+        out.flush();
+    }
+
+    void dropAbuses_mk3(final InputStream in, final OutputStream out, final String[] abuse) throws IOException {
+        try (final PrintStream writer = new PrintStream(out);
+             final BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            reader.lines()
+                    .map(s -> Arrays.stream(abuse)
+                            .reduce(s, (s1, s2) -> s1.replaceAll(s2, ""))
+                    ).forEach(writer::println);
+        }
     }
 }
