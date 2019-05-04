f14e729afffe319a606c1e45cd3713ec22a35ca2
Aleksey Romantsov <AlekseyRomantsov@yandex.ru>
2019-05-04 14:54:38 +0300

    add logger Log4j 2.11.2

--- chapter_003/src/main/java/ru/job4j/io/zip/Pack.java
+++ chapter_003/src/main/java/ru/job4j/io/zip/Pack.java
@@ -1,6 +1,7 @@
 package ru.job4j.io.zip;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,7 +24,7 @@ import java.util.zip.ZipOutputStream;
 public class Pack {
 
     private final Args args;
-    private final static Logger LOGGER = Logger.getLogger(Pack.class);
+    private final static Logger LOGGER = LogManager.getLogger(Pack.class);
 
     private Pack(Args args) {
          this.args = args;
